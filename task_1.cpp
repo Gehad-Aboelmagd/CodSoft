04bd478a38c0be91507d0dc34971f285499ee855
رب اشرح لي صدري و يسر لي امري <jhad44734@gmail.com>
2024-11-01 17:08:04 +0200

    Updating messages prompting user to increase/decrease their guess

--- task_1.cpp
+++ task_1.cpp
@@ -7,15 +7,15 @@ using namespace std;
 int main() {
     int generated_number;
     generated_number = rand() % 101;
-    cout << generated_number << endl;
+    // cout << generated_number << endl;
 
     int user_guess;
     cout << "Enter a number between 0 and 100: ";
     cin >> user_guess;
 
     while (user_guess != generated_number) {
-        (user_guess > generated_number) ? cout << "Higher! Try again: "
-                                        : cout << "Lower! Try again: ";
+        (user_guess > generated_number) ? cout << "Too high! Try again: "
+                                        : cout << "Too Low! Try again: ";
 
         cin >> user_guess;
     }
